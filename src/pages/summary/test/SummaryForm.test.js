2de5748e86afa130640708f7edea0b1b4bb74969
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2022-12-07 14:16:59 +0100

    SummaryForm correctly tested!

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,5 +1,8 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import SummaryForm from '../SummaryForm'
+import userEvent from '@testing-library/user-event'
+
+const user = userEvent.setup()
 
 describe('Comportamiento del boton con el checkbox', () => {
   test('Checkbox unchecked by default', () => {
@@ -11,20 +14,43 @@ describe('Comportamiento del boton con el checkbox', () => {
     expect(checkbox).not.toBeChecked()
     expect(button).toBeDisabled()
   })
-  test('Checking checkbox enables button', () => {
+  test('Checking checkbox enables button', async () => {
     render(<SummaryForm />)
     const button = screen.getByRole('button', { name: /comprar/i })
     const checkbox = screen.getByRole('checkbox', {
       name: /Accept terms and conditions/i,
     })
-    fireEvent.click(checkbox)
+    await user.click(checkbox)
     expect(checkbox).toBeChecked()
     expect(button).toBeEnabled()
 
     // Segundo click
-
-    fireEvent.click(checkbox)
+    await user.click(checkbox)
     expect(button).toBeDisabled()
     expect(checkbox).not.toBeChecked()
+  })
+})
+
+describe('pophover testing', () => {
+  test('Pophover responds to hover', async () => {
+    render(<SummaryForm />)
+    const nullPophover = screen.queryByText(
+      /No ice cream will actually be delivered/i
+    )
+    const termsAndConditions = screen.getByText(/terms and conditions/i)
+
+    // pophover starts out hidden
+    expect(nullPophover).not.toBeInTheDocument()
+
+    // pophover appears on mousehover of checkbox label
+    await user.hover(termsAndConditions)
+    const pophover = screen.getByText(
+      /No ice cream will actually be delivered/i
+    )
+    expect(pophover).toBeInTheDocument()
+
+    // pophover dissapear when mouse out
+    await user.unhover(termsAndConditions)
+    expect(pophover).not.toBeInTheDocument()
   })
 })
