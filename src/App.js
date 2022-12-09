0ef66973bd5ca36f6b9c444589ba30e8079112a7
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2022-12-09 11:26:27 +0100

    Test and code alert from server error response

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import SummaryForm from './pages/summary/SummaryForm'
+import Options from './pages/entry/Options'
+import OrderEntry from './pages/entry/OrderEntry'
 
 function App() {
   return (
     <div className='App'>
-      <SummaryForm />
+      <OrderEntry />
     </div>
   )
 }
